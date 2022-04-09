49f8da29ad290513cf7cd4f083134f49d42c371f
= <=>
2022-04-09 08:11:35 -0300

    aplicação da técnica SMOTE ao conjunto de dados desbalanceado

--- tutorial01/script02.py
+++ tutorial01/script02.py
@@ -9,26 +9,53 @@ from numpy import where
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
 
+
 # define dataset
-X, y = make_classification(n_samples=10_000, n_features=2, n_redundant=0, n_clusters_per_class=1, weights=[0.99],
-                           flip_y=0, random_state=1)
+def define_dataset():
+    X, y = make_classification(n_samples=10_000, n_features=2, n_redundant=0, n_clusters_per_class=1, weights=[0.99],
+                               flip_y=0, random_state=1)
+    return X, y
+
 
 # summarize class distribution
-counter = Counter(y)
-print(counter)
+def summarize_class_distribution(y):
+    return Counter(y)
 
-# transform the dataset
-oversample = SMOTE()
-
-X, y = oversample.fit_resample(X, y)
-
-# summarize the new class distribution
-counter = Counter(y)
-print(counter)
 
 # scatter plot of examples by class label
-for label, _ in counter.items():
-    row_ix = where(y == label)[0]
-    plt.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
-plt.legend()
-plt.show()
+def scatter_plot(X, y):
+    counter = summarize_class_distribution(y)
+    for label, _ in counter.items():
+        row_ix = where(y == label)[0]
+        plt.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
+    plt.legend()
+    plt.show()
+
+
+# transform the dataset
+def transform_dataset(X, y):
+    over = SMOTE()
+    X, y = over.fit_resample(X, y)
+    return X, y
+
+'''
+MAIN
+'''
+
+# define dataset
+X, y =  define_dataset()
+
+# summarize class distribution
+print(summarize_class_distribution(y))
+
+# scatter plot of examples by class label
+scatter_plot(X, y)
+
+# transform the dataset
+X, y = transform_dataset(X, y)
+
+# summarize class distribution
+print(summarize_class_distribution(y))
+
+# scatter plot of examples by class label
+scatter_plot(X, y)
