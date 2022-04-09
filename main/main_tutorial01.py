2d949eb390d2ae08d19bb448f3481b96339c5e8b
= <=>
2022-04-09 16:55:47 -0300

    técnica SMOTE para problema de classificação binária em conjunto de dados desbalanceado

---
+++ main/main_tutorial01.py
@@ -0,0 +1,148 @@
+from tutorial.tutorial01 import Tutorial
+
+t = Tutorial()
+
+'''
+Generate and plot a synthetic imbalanced classification dataset
+'''
+print('\nGenerate and plot a synthetic imbalanced classification dataset')
+
+# create dataset
+X_original, y_original = t.create_dataset()
+
+# summarize class distribution
+counter = t.create_summarize(y_original)
+print(counter)
+
+# scatter plot of examples by class label
+t.create_plot(X_original, y_original)
+
+methods = ['smote', 'borderline_smote', 'svm_smote', 'adasyn']
+
+'''
+Oversample and plot imbalanced dataset with method
+'''
+for o in methods:
+    print('\nOversample and plot imbalanced dataset with {}'.format(o))
+
+    # over-sampling
+    over = t.create_over_sampling(o)
+
+    # create pipeline
+    steps = [over]
+    pipeline = t.create_pipeline(steps)
+
+    # transform the dataset
+    X, y = t.transform_dataset(pipeline, X_original, y_original)
+
+    # summarize
+    counter = t.create_summarize(y)
+    print(counter)
+
+    # scatter plot of examples by class label
+    t.create_plot(X, y)
+
+'''
+Oversample with SMOTE and random undersample for imbalanced dataset
+'''
+for o in methods:
+    print('\nOversample with {} and random undersample for imbalanced dataset'.format(o))
+
+    # over-sampling
+    over = t.create_over_sampling(o, 0.1)
+
+    # under-sampling
+    under = t.create_under_sampling('ramdom_under_sampler', 0.9)
+
+    # create pipeline
+    steps = [over, under]
+    pipeline = t.create_pipeline(steps)
+
+    # transform the dataset
+    X, y = t.transform_dataset(pipeline, X_original, y_original)
+
+    # summarize
+    counter = t.create_summarize(y)
+    print(counter)
+
+    # scatter plot of examples by class label
+    t.create_plot(X, y)
+
+'''
+Decision tree evaluated on imbalanced dataset
+'''
+print('\nDecision tree evaluated on imbalanced dataset')
+
+# create model
+model = t.create_model('decision_tree')
+
+# create pipeline
+steps = [model]
+pipeline = t.create_pipeline(steps)
+
+# evaluate model
+t.evaluate(pipeline, X_original, y_original)
+
+'''
+Decision tree evaluated on imbalanced dataset with method oversampling
+'''
+for o in methods:
+    print('\nDecision tree evaluated on imbalanced dataset with {} oversampling'.format(o))
+
+    # over-sampling
+    over = t.create_over_sampling(o)
+
+    # create model
+    model = t.create_model('decision_tree')
+
+    # create pipeline
+    steps = [over, model]
+    pipeline = t.create_pipeline(steps)
+
+    # evaluate model
+    t.evaluate(pipeline, X_original, y_original)
+
+
+'''
+Decision tree evaluated on imbalanced dataset with method oversampling and random undersampling
+'''
+for o in methods:
+    print('\nDecision tree evaluated on imbalanced dataset with {} oversampling and random undersampling'.format(o))
+
+    # over-sampling
+    over = t.create_over_sampling(o, 0.1)
+
+    # under-sampling
+    under = t.create_under_sampling('ramdom_under_sampler', 0.9)
+
+    # create model
+    model = t.create_model('decision_tree')
+
+    # create pipeline
+    steps = [over, under, model]
+    pipeline = t.create_pipeline(steps)
+
+    # evaluate model
+    t.evaluate(pipeline, X_original, y_original)
+
+'''
+Grid search k value for method oversampling and random undersampling for imbalanced classification
+'''
+for o in methods:
+    print('\nGrid search k value for {} oversampling and random undersampling for imbalanced classification'.format(o))
+    for k in range(1, 8):
+        # over-sampling
+        over = t.create_over_sampling(o, 0.1, k)
+
+        # under-sampling
+        under = t.create_under_sampling('ramdom_under_sampler', 0.9)
+
+        # create model
+        model = t.create_model('decision_tree')
+
+        # create pipeline
+        steps = [over, under, model]
+        pipeline = t.create_pipeline(steps)
+
+        # evaluate model
+        t.evaluate(pipeline, X_original, y_original, k)
---
+++ tutorial/tutorial01.py
@@ -0,0 +1,94 @@
+'''
+title: SMOTE for Imbalanced Classification with Python
+reference: https://machinelearningmastery.com/smote-oversampling-for-imbalanced-classification/
+'''
+
+
+from collections import Counter
+
+from numpy import where, mean, std
+
+import matplotlib.pyplot as plt
+
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import BorderlineSMOTE #cria amostras no limite de decisão entre as duas classes
+from imblearn.over_sampling import SVMSMOTE
+from imblearn.over_sampling import ADASYN
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
+
+from sklearn.datasets import make_classification
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import cross_val_score
+
+
+class Tutorial:
+
+    # create dataset
+    def create_dataset(self):
+        X, y = make_classification(n_samples=10_000, n_features=2, n_redundant=0, n_clusters_per_class=1,
+                                   weights=[0.99],
+                                   flip_y=0, random_state=1)
+        return X, y
+
+
+    # summarize class distribution
+    def create_summarize(self, y):
+        return Counter(y)
+
+
+    # scatter plot of examples by class label
+    def create_plot(self, X, y):
+        counter = self.create_summarize(y)
+        for label, _ in counter.items():
+            row_ix = where(y == label)[0]
+            plt.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
+        plt.legend()
+        plt.show()
+
+
+    # create model decision tree
+    def create_model(self, name):
+        if name == 'decision_tree':
+            model = DecisionTreeClassifier()
+        return ('model', model)
+
+
+    # oversampling
+    def create_over_sampling(self, name, ss='auto', k_n=5):
+        if name == 'smote':
+            over = SMOTE(sampling_strategy=ss, k_neighbors=k_n)
+        elif name == 'borderline_smote':
+            over = BorderlineSMOTE(sampling_strategy=ss, k_neighbors=k_n)
+        elif name == 'svm_smote':
+            over = SVMSMOTE(sampling_strategy=ss, k_neighbors=k_n)
+        elif name == 'adasyn':
+            over = ADASYN(sampling_strategy=ss, n_neighbors=k_n)
+        return ('over', over)
+
+
+    # undersample random
+    def create_under_sampling(self, name, ss='auto'):
+        if name == 'ramdom_under_sampler':
+            under = RandomUnderSampler(sampling_strategy=ss)
+        return ('under', under)
+
+
+    # create pipeline
+    def create_pipeline(self, steps):
+        pipeline = Pipeline(steps=steps)
+        return pipeline
+
+
+    # transform the dataset
+    def transform_dataset(self, pipeline, X, y):
+        X, y = pipeline.fit_resample(X, y)
+        return X, y
+
+
+    # evaluate
+    def evaluate(self, pipeline, X, y, k=5):
+        cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=30, random_state=1)
+        scores = cross_val_score(pipeline, X, y, scoring='roc_auc', cv=cv, n_jobs=-1)
+        print('k = {}, Mean ROC AUC: {:.2f}% ({:.2f}%)'.format(k, (mean(scores) * 100), (std(scores) * 100)))
